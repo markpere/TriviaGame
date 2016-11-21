a161c1732429320cd659f3d51a62f2565efbca9b
Mark Pereira <markpereira@marks-mbp.attlocal.net>
2016-11-20 19:00:55 -0600

    adding the json and php files

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//Set up the document.ready so that this all works
+$(document).ready(function(){
+
+	$("#start").on("click", function(){
+
+	$("#hiddenText").show();
+	$("#start").hide();
+	$('#wizard').get(0).play();
+
+}); //ends start on click function
+
+	$("#submitButton").on("click", function(){
+		
+	}); //ends submit button click function
+
+}) //ends document.ready
+
+//Set up the event listener for the button
+
