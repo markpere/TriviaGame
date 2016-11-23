72c07ec80a10ce24b9bc0e58bcafc799dc83a32f
Mark Pereira <markpereira@marks-mbp.attlocal.net>
2016-11-22 20:13:32 -0600

    Updatd files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,16 @@
 //Set up the document.ready so that this all works
+APP = {};
+
+APP.Globals = {
+   answers: {myQuestion1:'Cupboard',
+            myQuestion2:'Hufflepuff',
+        	myQuestion3: 'Pear',
+        	myQuestion4: 'Vincent',
+        	myQuestion5: 'The Worst Birthday',
+        	myQuestion6: 'House-Elf',
+        	myQuestion7: 'David Yates'}           
+}
+
 $(document).ready(function(){
 
 	$("#start").on("click", function(){
@@ -10,8 +22,31 @@ $(document).ready(function(){
 }); //ends start on click function
 
 	$("#submitButton").on("click", function(){
-		
+		$("#trivForm").submit();
 	}); //ends submit button click function
+   
+   $("#trivForm").on('submit', function(e){
+      e.preventDefault();
+      var $allInputs = $('#trivForm input');
+      $allInputs.each(function(){
+          var $this = $(this);
+          $this.parent().css('color', '#ffffff');
+      });
+      var $selectedInputs = $('#trivForm input:checked');
+      $selectedInputs.each(function(){
+         var $this = $(this);
+         var name = $this.attr('name');
+         var val = $this.val();
+            if(APP.Globals.answers[name]==val){
+               $this.parent().css('color', '#00ff00');
+            }else{
+               $this.parent().css('color', '#ff0000');
+               
+            }
+      });
+      console.info($selectedInputs);
+		
+	}); //ends form submit function
 
 }) //ends document.ready
 
