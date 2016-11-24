0f805054ff7b550b6a45a174620cd0dd192277e3
kashan qazi <kashan.qazi02@utexas.edu>
2016-11-24 14:14:38 -0600

    fixed javascript to get the time to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,44 +18,51 @@ $(document).ready(function(){
     $("#start").hide();
     $('#wizard').get(0).play();
 
-}); //ends start on click function
 
   //Now I need to start the timer
   setTimeout (oneSecond, 1000);
   function oneSecond(){
     var counter;
     var stopwatch = {
+      
       time: 0,
+      
+      start : function() {
+    
+        counter = setInterval(stopwatch.count, 1000);
+        
+      },
+    
+      count: function() {
+        stopwatch.time++;
+        var converted = stopwatch.timeConverter(stopwatch.time);
+        console.log(converted);
+        $("#display").html(converted);
+      },
+    
+      timeConverter: function(t) {
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+        if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+        if (minutes === 0) {
+          minutes = "00";
+        } 
+        else if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+
+        return minutes + ":" + seconds;
+      }
     };
-    start: function() {
-    
-    counter = setInterval(stopwatch.count, 1000);
-    },
-    
-    count: function() {
-      stopwatch.time++;
-      var converted = stopwatch.timeConverter(stopwatch.time);
-      console.log(converted);
-      $("#display").html(converted);
-    },
-    timeConverter: function(t) {
-      var minutes = Math.floor(t / 60);
-      var seconds = t - (minutes * 60);
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-      if (minutes === 0) {
-        minutes = "00";
-      } 
-      else if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-      return minutes + ":" + seconds;
-    }
-  };
 
-  } //ends the oneSecond function
+    stopwatch.start()
 
+  }; //ends the oneSecond function
+
+
+}); //ends start on click function
   
 
 	$("#submitButton").on("click", function(){
